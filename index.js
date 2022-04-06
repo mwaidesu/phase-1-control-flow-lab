be08c771a98afa71ab50ce365278ccf2a9bd3002
Mwai <machariamwai@gmail.com>
2022-04-06 11:30:01 +0300

    Add if else statements, ternary operator and switch statement

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!'
+  }
+  else if (feet > 400 && feet <= 2000) {
+    result = 'That will be twenty bucks.'
+  }
+
+  else if (feet >= 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+
+  else if (feet > 2500) {
+    result = 'No can do.'
+  }
+
+
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  let result;
+
+  city === "NYC"? (result = "Ok, sounds good."):(result ="No go.")
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+
+  let result
+
+  switch(tip){
+    case 'generous':
+      result = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      result = "Thank you."
+      break;
+    default:
+      result = "Bye."
+      break;
+  }
+  return result;
 }
